9870f963904badc37f3eac2013976cc7135fb26e
Aaron <aaron.hook@propelleraero.com.au>
2025-12-03 13:36:59 +1100

    Template updates

--- src/template.hs
+++ src/template.hs
@@ -1,11 +1,19 @@
 import System.Environment (getArgs)
-import AoCUtils
+import System.CPUTime
+import Text.Printf
 
 main :: IO ()
 main = do 
   [filename] <- getArgs
   content <- readFile filename
-  print $ solve $ lines content
+  
+  start <- getCPUTime
+  let result = solve $ lines content
+  end <- getCPUTime
+  
+  let diff = fromIntegral (end - start) / (10^12)
+  printf "Result: %s\n" (show result)
+  printf "Execution time: %.3f seconds\n" (diff :: Double)
 
 -- Puzzle solution below
 
