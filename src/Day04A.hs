8c42a3ba03c747baec292ba670ccb413c001ab46
Aaron <aaron.hook@propelleraero.com.au>
2025-12-04 16:55:00 +1100

    Day 4

--- src/Day04A.hs
+++ src/Day04A.hs
@@ -1,7 +1,20 @@
 module Day04A (solve) where
+import AoCUtils (parseGrid, Grid, Coord, neighbors8)
+import Data.Map (toList) 
+import qualified Data.Map as Map
 
 type Input = [String]
 type Output = Int
 
 solve :: Input -> Output
-solve _ = 0
+solve input =
+  let grid = parseGrid input
+      validPaperRolls = filter (sorroundedByLessThanFour grid) (toList grid)
+  in length validPaperRolls
+
+sorroundedByLessThanFour :: Grid -> (Coord, Char) -> Bool
+sorroundedByLessThanFour _ (_, '.') = False 
+sorroundedByLessThanFour grid (coord, _) =
+  let neighbors = neighbors8 coord
+      occupiedNeighbors = filter (\c -> Map.lookup c grid == Just '@') neighbors
+  in length occupiedNeighbors < 4
--- src/Day04B.hs
+++ src/Day04B.hs
@@ -1,7 +1,28 @@
 module Day04B (solve) where
+import AoCUtils (parseGrid, Grid, Coord, neighbors8)
+import Data.Map (toList) 
+import qualified Data.Map as Map
 
 type Input = [String]
 type Output = Int
 
 solve :: Input -> Output
-solve _ = 0
+solve input =
+  let grid = parseGrid input
+      totalRemovals = countValidPaperRolls grid 
+  in totalRemovals
+
+sorroundedByLessThanFour :: Grid -> (Coord, Char) -> Bool
+sorroundedByLessThanFour _ (_, '.') = False 
+sorroundedByLessThanFour grid (coord, _) =
+  let neighbors = neighbors8 coord
+      occupiedNeighbors = filter (\c -> Map.lookup c grid == Just '@') neighbors
+  in length occupiedNeighbors < 4
+
+countValidPaperRolls :: Grid -> Int 
+countValidPaperRolls grid =
+  let validPaperRolls = filter (sorroundedByLessThanFour grid) (toList grid)
+  in if null validPaperRolls
+     then 0
+     else let newGrid = foldr (\(coord, _) g -> Map.insert coord '.' g) grid validPaperRolls
+          in length validPaperRolls + countValidPaperRolls newGrid
