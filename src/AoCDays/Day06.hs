aec8f54ca7a45a2c4e8d65f2f603b691c62a3cc1
Aaron <aaron.hook@propelleraero.com.au>
2025-12-06 20:20:13 +1100

    Day 6

--- src/AoCDays/Day06.hs
+++ src/AoCDays/Day06.hs
@@ -1,10 +1,37 @@
 module AoCDays.Day06 (partA, partB) where
+import Data.List (transpose)
+import Text.Regex.Posix (AllTextMatches(getAllTextMatches), (=~))
 
 type Input = [String]
 type Output = Int
 
 partA :: Input -> Output
-partA _ = 0
+partA input = 
+  let (matrix, symbols) = splitAt (length input - 1) input
+      parsedMatrix = map extractNumbers matrix
+      columns = transpose parsedMatrix
+      symbolsWithValues = zip (words $ head symbols) columns
+  in sum $ map applyOperation symbolsWithValues
 
 partB :: Input -> Output  
-partB _ = 0
+partB input = 
+  let (matrix, symbols) = splitAt (length input - 1) input
+      columns = transpose matrix 
+      groupedColumns = splitOnSpaces columns
+      parsedColumns = map (concatMap extractNumbers) groupedColumns
+      symbolsWithValues = zip (words $ head symbols) parsedColumns
+  in sum $ map applyOperation symbolsWithValues
+
+extractNumbers :: String -> [Int]
+extractNumbers input = map read $ getAllTextMatches (input =~ "[0-9]+")
+
+applyOperation :: (String, [Int]) -> Int 
+applyOperation (symbol, values) = case symbol of
+  "+" -> sum values
+  "*" -> product values
+  _ -> error "Unknown operation"
+
+splitOnSpaces :: [String] -> [[String]]
+splitOnSpaces xs = case break (all (== ' ')) xs of
+  (chunk, []) -> [chunk | not (null chunk)]
+  (chunk, _:rest) -> [chunk | not (null chunk)] ++ splitOnSpaces rest
